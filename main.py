21d864f485308f96ab318622ccf668fe41f34d39
Aadit G <31579510+aaditg@users.noreply.github.com>
2022-04-07 11:27:10 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,56 @@ import math
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+from dbhelper import *
 
 f = open('StateSaver.txt', 'r')
 contents= f.read()
 print("Your State Is Currently: " + str(contents))
 stateG = str(contents)
+
+
+
+ 
+def login():
+    print("\n")
+    print("-------LOGIN-------")
+    username = input("Username: ")
+    password = input("Password: ")
+    signup = input("Signup? (y(add accounut)/n(login)): ")
+    if signup in ["y", "Y", "Yes", "yes"]:
+        email = input("Email: ")
+        community = input("Community: ")
+        try:
+            add_df(email, username, community_to_ID(community), password)
+            print("OK!")
+            return 1
+        except:
+            print("error signing in")
+            login()
+    elif signup in ["n", "N", "No", "no"]:
+        if isUser(username, password):
+            print('OK!')
+            return 1
+        else:
+            print("Incorrect Login Information")
+            login()
+    else: 
+        print('please type yes or no')
+        
+
+
+def Dashboard():
+    print("Dash")
+    return 1
+
+def Community_View():
+    print("Community_View")
+
+login_value = login()
+if login_value == 1:
+    dash_value = Dashboard()
+    if dash_value == 1:
+        comm = Community_View()
 
 def getDateAndFormat(backDays):
   today = date.today()
